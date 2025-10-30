015ce2703e0d1adbcf5a6c5c6485a2b3ec8af609
nurhaalizaa <107898047+nurhaalizaa@users.noreply.github.com>
2025-10-30 14:04:10 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import home, input_data, Lainnya, coba
+
+# Sidebar menu
+st.sidebar.image("menu.png",width=700)
+st.sidebar.markdown(
+    "<h2 style='text-align: center;'>Desa Pangarengan</h2>", 
+    unsafe_allow_html=True
+)
+
+menu = st.sidebar.selectbox(
+    "Menu Aplikasi",
+    ["Beranda", "Input Data"],
+    label_visibility="collapsed"
+)
+
+# Routing ke halaman
+if menu == "Beranda":
+    home.app()
+elif menu == "Input Data":
+    input_data.app()
+#elif menu == "Lainnya":
+ #   Lainnya.app()
+
+
